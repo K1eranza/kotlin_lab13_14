0e2eeb6ad1b219d96be012dfe333ec7bbecba3fd
K1eranza <zubrevaevelina20641@gmail.com>
2026-02-11 10:25:36 +0300

    fold() func

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -51,17 +51,10 @@ val cookies = listOf(
     )
 )
 fun main() {
-    val groupedMenu = cookies.groupBy { it.softBaked }
-    val softBakedMenu = groupedMenu[true] ?: emptyList()
-    val crunchyMenu = groupedMenu[false] ?: emptyList()
-    println("Soft cookies:")
-    softBakedMenu.forEach {
-        println("${it.name} - $${it.price}")
+    val totalPrice = cookies.fold(initial = 0.0) {total, cookie -> total + cookie.price
     }
-    println("Crunchy cookies:")
-    crunchyMenu.forEach {
-        println("${it.name} - $${it.price}")
-    }
+println("Total price: $${totalPrice}")
 }
+
 
 
